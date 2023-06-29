d0423c5f2343bf4ecb192ade48b2f03208290079
David8072 <136148509+David8072@users.noreply.github.com>
2023-06-29 21:58:18 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,58 @@
+let myResume={
+    "basics": {
+      "name": "DAVID BERNAHAM P",
+      "email": "davidp9092@gamil.com",
+      "phone": 8072396638,
+      "degree": "Diploma in Automobile",
+      "location": {
+        "address": "11/A Vikash Illam Lakashmi Garden Kondayampalayam",
+        "postalCode": 641110,
+        "city": "Coimbatore",
+        "state": "Tamilnadu",
+        "country": "India"
+      },
+      "profiles": [
+        {
+          "github":"https://github.com/David8072"
+        }
+      ]
+    },
+    "work": [
+      {
+        "company": "Roots Cast PVT",
+        "position": "Costing Associate",
+        "startDate": "2022-03-20",
+        "endDate": "2023-06-17",
+        "summary": "i am the Costing Associate handling Engineering Project task and gaining  experience",
+      },
+    ],
+    "education": [
+      {
+        "institution": "Sri Ramakrishna Polytechnic College",
+        "department": "Automobile",
+        "studyType": "fulltime",
+        "batch start year": 2015,
+        "batch end year": 2018,
+      }
+    ],
+    "skills": [
+      {
+        "name": "Mono Wheel Vehicle",
+        "project": [
+          "The driver and the engine anchor the inner wheel, while the engine propels the outer loop"
+        ]
+      }
+    ],
+    "languages": [
+      {
+        "language": "Tamil,Enlish",
+      }
+    ],
+    "interests": [
+      {
+        "name": "Coder",
+      }
+    ]
+  }
+  console.log(myResume);
+  
